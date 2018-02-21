965acf6dd7288e4785e4950c780de719de4a6678
Erik Hartley <erik>
2018-02-21 10:04:55 -0700

    initial commit

---
+++ WindowsAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindowsAnimation
+//
+//  Created by Erik HARTLEY on 2/21/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
